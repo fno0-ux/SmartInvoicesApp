b4f5c13e6861c928caf5877f9608b919ccd77ac4
Ali-ux <fno0o.m.505@gmail.com>
2025-10-29 14:13:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('smart-invoices-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/login.html',
+        '/register.html',
+        '/styles.css',
+        '/app.js',
+        '/auth.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
