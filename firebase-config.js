929ae03e3e1038c032d108fa5f642aa637d83f6d
Ali-ux <fno0o.m.505@gmail.com>
2025-11-14 15:46:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,21 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBAWj23fIad9ec9T1fbT0XkpVLxnK9f2wg",
+  authDomain: "smartinvoicesapp-ca382.firebaseapp.com",
+  projectId: "smartinvoicesapp-ca382",
+  storageBucket: "smartinvoicesapp-ca382.appspot.com",
+  messagingSenderId: "578187855094",
+  appId: "1:578187855094:web:5e4414db92c5ad17afd8d7",
+  measurementId: "G-T1H667FSH3"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
