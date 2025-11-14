51a69adc76eae141f0cbe2462d904155ea61c694
Ali-ux <fno0o.m.505@gmail.com>
2025-11-14 15:00:53 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
